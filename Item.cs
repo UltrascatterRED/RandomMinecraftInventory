7967e29a0a946a90503ef51f3707820e34ce558a
UltrascatterRED <natbarton14@gmail.com>
2024-02-28 23:57:19 -0500

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMinecraftInventory
+{
+	internal class Item
+	{
+		public string Name { get; set; }
+		public string Namespace_ID { get; set; }
+		public string Description { get; set; }
+		public string ImagePath { get; set; }
+		public int StackSize { get; set; }
+		public bool IsRenewable { get; set; }
+		public Item() 
+		{ 
+			Name = string.Empty;
+			Namespace_ID = string.Empty;
+			Description = string.Empty;
+			ImagePath = string.Empty;
+			StackSize = 0;
+			IsRenewable = false;
+		}
+		public Item(string name, string namespace_ID, string description, string imagePath, int stackSize, bool isRenewable)
+		{
+			Name = name;
+			Namespace_ID = namespace_ID;
+			Description = description;
+			ImagePath = imagePath;
+			StackSize = stackSize;
+			IsRenewable = isRenewable;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace RandomMinecraftInventory
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			MainMenu();
+		}
+		public static void MainMenu()
+		{
+			Console.Clear();
+			Console.BackgroundColor = ConsoleColor.DarkGreen;
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.Write
+			(
+				"+------------------------------------------+\n" +
+				"|   Minecraft Random Inventory Generator   |\n" +
+				"+------------------------------------------+\n"
+			);
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.ForegroundColor = ConsoleColor.White;
+
+			Console.WriteLine("[1] Generate New Inventory");
+			Console.WriteLine("[2] View Inventory (concise)");
+			Console.WriteLine("[3] View Inventory (detailed)");
+			// extra options (if time allows)
+            // Console.WriteLine("[4] Save Inventory To File");
+            // Console.WriteLine("[5] Load Inventory From File");
+			// end extra options
+            Console.WriteLine("[4] How To Use This App");
+            Console.WriteLine("[5] Exit");
+            Console.WriteLine();
+			Console.Write(">>> ");
+        }
+		public static void AppTutorial()
+		{
+			Console.Write
+			(
+				"This app is pretty simple, and as such, uses simple commands.\n" +
+				"The commands are as follows:\n" +
+				"<0-9>..............Selects and executes the corresponding menu option (i.e. '3')." +
+				"<B/b>..............Backs out to the previous menu. Case-insensitive."
+			);
+			Console.WriteLine();
+			Console.Write(">>> ");
+		}
+		// public static Inventory GenerateInventory()
+		// public static void PromptAndValidateInput()
+	}
+}
